be1c126346f2f03f4d74acf537f6fd7e7fe2a7a7
Jiwon <sjiwon9303@gmail.com>
2022-03-08 15:57:03 +0900

    test

---
+++ src/Common/Content.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Footer from '../Common/Footer';
+import Header from '../Common/Header';
+//s
+const Content = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+            {/* <container className ="main-content">
+            <div className="flexbox">
+            <div className="item"><span>content1</span></div>
+            <div className="item"><span>content2</span></div>
+            <div className="item"><span>content3</span></div>
+            <div className="item"><span>content4</span></div>
+            <div className="item"><span>content5</span></div>
+            <div className="item"><span>content6</span></div>
+            </div>
+            </container> */}
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default Content;
