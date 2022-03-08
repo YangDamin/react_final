5a3291c4a83ab0c60633f5733913b7f85ffbadc7
Jiwon <sjiwon9303@gmail.com>
2022-03-08 17:47:16 +0900

    front

--- src/Common/Content.js
+++ src/Common/Content.js
@@ -1,29 +1,74 @@
-import React from 'react';
-import Footer from '../Common/Footer';
-import Header from '../Common/Header';
-<<<<<<< HEAD
-//see
-=======
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
 
->>>>>>> main
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(14),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
 const Content = () => {
-    return (
-        <div>
-            <Header></Header>
-            
-            {/* <container className ="main-content">
-            <div className="flexbox">
-            <div className="item"><span>content1</span></div>
-            <div className="item"><span>content2</span></div>
-            <div className="item"><span>content3</span></div>
-            <div className="item"><span>content4</span></div>
-            <div className="item"><span>content5</span></div>
-            <div className="item"><span>content6</span></div>
-            </div>
-            </container> */}
-            <Footer></Footer>
-        </div>
-    );
-}
+  return (
+    <>
+    <CssBaseline />
+      <Container className="content-container" fixed>
+        <Box sx={{ bgcolor: 'rgba(238, 238, 238, 1)',borderRadius:'40px 40px 0 0',borderStyle:'solid',borderColor:'rgba(153, 153, 153, 1)', height: '100vh' }}>
+      <Box sx={{ flexGrow: 1,mt:6 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Item>video1</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>video2</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>video3</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>video4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>video5</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>video6</Item>
+          </Grid>
+        </Grid>
+      </Box>
+      </Box>
+      </Container>
+    </>
+  );
+};
 
 export default Content;
+
+// import React from 'react';
+// import Footer from '../Common/Footer';
+// import Header from '../Common/Header';
+
+// const Content = () => {
+//     return (
+//         <>
+//             <container className ="main-content">
+//             <div className="flexbox">
+//             <div className="item"><span>content1</span></div>
+//             <div className="item"><span>content2</span></div>
+//             <div className="item"><span>content3</span></div>
+//             <div className="item"><span>content4</span></div>
+//             <div className="item"><span>content5</span></div>
+//             <div className="item"><span>content6</span></div>
+//             </div>
+//             </container>
+//         </>
+//     );
+// }
+
+// export default Content;
