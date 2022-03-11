bfbc9de7fc96e52606dbefa16fec00f7df47c9c3
Jiwon <sjiwon9303@gmail.com>
2022-03-11 09:42:43 +0900

    메인페이지 생성

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,14 +3,12 @@ import Header from '../Common/Header';
 import Nav from '../Common/Nav';
 import Body from '../Common/Body';
 import Footer from '../Common/Footer';
-import Content from '../Common/Content';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Nav></Nav>
-            <Content></Content>
             <Body></Body>
             <Footer></Footer>
         </div>
