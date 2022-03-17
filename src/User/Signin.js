d5f354b61aea661d3d37868274c968d7d6d1e848
haeyoungim <wwkoi@naver.com>
2022-03-17 12:33:56 +0900

    수정

--- src/User/Signin.js
+++ src/User/Signin.js
@@ -1,23 +1,29 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import Footer from '../Common/Footer';
 import './Signin.css';
 import {
   CardHeader,
   Button,
-  Form,
-  FormGroup,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Input,
 }
   from 'reactstrap';
-import Swal from 'sweetalert2';
 
 
 const Signin = () => {
 
+  const navigate = useNavigate();
 
+  // 중복확인 하기 위해
+  const [userList, setUserList] = useState([]);
+
+
+  // function hendleClick(e) {
+  //     e.preventDefault();
+  //     window.location.href = "/users/signup"
+
+  //   }
 
   function GithubClick(e) {
     e.preventDefault();
@@ -28,57 +34,81 @@ const Signin = () => {
 
   function KakaoClick(e) {
     e.preventDefault();
-    window.location.href = "https://kauth.kakao.com/oauth/authorize?client_id=04c9a760d057d6ccbc3cdb399201" +
-      "c2a3&redirect_uri=http://localhost:3000/&response_type=code"
+    window.location.href = "https://kauth.kakao.com/oauth/authorize?client_id=bb1062f029aa6ff58bbe4fc11289" +
+      "458c&redirect_uri=http://localhost:3000/&response_type=code"
 
   }
 
 
+  function SendData(e) {
+    e.preventDefault();
+    console.log(e);
+    console.log(e.target['0'].value);
+    console.log(e.target['1'].value);
+
+    const formData = new FormData();
+    const email = e
+      .target['0']
+      .value;
+    const pwd = e
+      .target['1']
+      .value;
+
+    formData.append("email", email);
+    formData.append("pwd", pwd);
+
+    axios({ url: 'http://localhost:8080/api/log', method: 'post', data: formData }).then(
+      function (res) {
+        console.log(res.data);
+
+        if (res.data.code === 200) {
+          if (res.data.Code.id === 1) {
+            sessionStorage.setItem('Managername', res.data.name);
+            // Swal.fire({ icon: 'success', title: 'Good!', text: 'Login complete!' })
+
+            setTimeout(function () {
+              window.location = '/Manager/index';
+            }, 1000);
+
+          } else {
+            sessionStorage.setItem('name', res.data.name);
+            // Swal.fire({ icon: 'success', title: 'Good!', text: 'Login complete!' })
+            setTimeout(function () {
+              window.location = '/';
+            }, 1000);
+
+          }
+        } else if (res.data.code === 201) {
+          //   Swal.fire(
+          //     { icon: 'error', title: 'Oops...', text: 'Please check your email and password..' }
+
+          //   )
+
+        }
+      }
+    )
+  }
+
   return (
-    <>
-    <Header/>
     <div>
       <div class="container" id="signup_container">
         <h3 class="h3 mt-5 mb-4">로그인</h3>
         <hr class="border-dark mt-5 mb-4" />
 
         <div class="row" style={{ textAlign: 'left' }}>
-        <Form role="form" onSubmit={SendData}>
-          {/* <form> */}
-
-
+          <form>
+            {/* 이메일 */}
             <div class="form-group mb-3">
               <div class="row">
                 <div class="col-3 text-left">
                   <label>이메일</label><span style={{ color: 'red' }}> *</span>
                 </div>
                 <div class="col-6">
-             {/* 이메일 */}
-            <FormGroup>
-            <InputGroup>
-              {/* <InputGroupAddon addonType="prepend"> */}
-                <InputGroupText>
-                  <i className="ni ni-email-83" />
-                </InputGroupText>
-              {/* </InputGroupAddon> */}
-              <Input
-                placeholder="Email"
-                type="email"
-                id="email"
-                name="email"
-                autoComplete="new-email" />
-            </InputGroup>
-          </FormGroup>
+                  <input class="form-control" type="email" name="email" id="email" placeholder="@까지 정확하게 입력해주세요." required />
                 </div>
 
               </div>
             </div>
-
-
-
-
-
-            
 
             {/* 비밀번호 */}
             <div class="form-group mb-3">
@@ -87,7 +117,7 @@ const Signin = () => {
                   <label>비밀번호</label><span style={{ color: 'red' }}> *</span>
                 </div>
                 <div class="col-6">
-                  <input class="form-control" type="password" name="password" id="password" placeholder="비밀번호를 입력해주세요." required />
+                  <input class="form-control" type="password" name="password" id="pwd" placeholder="비밀번호를 입력해주세요." required />
                 </div>
               </div>
             </div>
@@ -95,114 +125,93 @@ const Signin = () => {
             <div style={{ textAlign: 'center' }}>
               <input type="submit" value="로그인" class="btn bg-secondary text-white flex-shrink-0 me-2"
                 onClick={(e) => {
-                  e.preventDefault();
 
                   const formData = new FormData();
                   //OOO님 안녕하세요!,로그인 성공이면 main페이지
                   //잘못된 정보를 입력하셨습니다. 로그인 실패하면 signin페이지
                   const email = document.getElementById("email").value;
                   const password = document.getElementById("password").value;
-                  
                   formData.append("email", email);
-                  formData.append("pwd", password);
-
-                  axios({
+                  formData.append("password", password);
+                  const result = axios({
                     url: 'http://localhost:8080/users/signin',
                     method: 'post',
                     data: formData
-                  }).then((res) => {
+                  });
+                  result.then((res) => {
                     const result = res.data;
                     if (result.code == 200) {
                       //로그인 성공하였을 때, SessionStorage에 값 넣고 
+                      sessionStorage.setItem("userId", result.user.id);
                       sessionStorage.setItem("email", result.user.email);
-                      sessionStorage.setItem("password", result.user.pwd);
-                      sessionStorage.setItem("name", result.user.name);
-                      
-                      Swal.fire(
-                        '',
-                        '로그인 성공',
-                        'success'
-                      )
-                    setTimeout(function(){
-                        window.location = '/';
-                    },2000)
-
+                      sessionStorage.setItem("password", result.user.password);
+                      console.log(sessionStorage.getItem("userId"), sessionStorage.getItem("email"), sessionStorage.getItem("pwd"));
+                      alert(result.msg);
+                      navigate("/");
                     } else if (result.code == 400) {
-                      Swal.fire({
-                        icon: 'error',
-                        text: '로그인 실패! 다시 입력해주세요.'
-                      })
+                      alert(result.msg);
                       document.getElementById("email").value = "";
                       document.getElementById("password").value = "";
                     }
-                  }).catch( (error) => {
-                    console.log(error);
-                  })
-
-                  
+                  });
                 }}></input>
 
 
               <a class="btn bg-secondary text-white flex-shrink-0 me-2" href="/users/signup">
                 <small>회원가입</small>
               </a>
-
             </div>
-
-
-
-
-
             <CardHeader className="bg-transparent">
-              <div className="text-muted text-center mt-2 mb-3">
-                <small>Sign in with</small>
-              </div>
-              <div className="btn-wrapper text-center">
+                <div className="text-muted text-center mt-2 mb-3">
+                  <small>Sign in with</small>
+                </div>
+                <div className="btn-wrapper text-center">
 
-                {/* Github Login  */}
-                <Button
-                  className="btn-neutral btn-icon"
-                  color="default"
-                  href="#pablo"
-                  onClick={GithubClick}>
-                  <span className="btn-inner--icon">
-                    <img
-                      alt="..."
-                      src={require("../assets/img/github.svg").default
-                      } />
-                  </span>
-                  <span className="btn-inner--text">Github</span>
-                </Button>
+                  {/* Github Login  */}
+                  <Button
+                    className="btn-neutral btn-icon"
+                    color="default"
+                    href="#pablo"
+                    onClick={GithubClick}>
+                    <span className="btn-inner--icon">
+                      <img
+                        alt="..."
+                        src={require("../assets/img/github.svg").default
+                        } />
+                    </span>
+                    <span className="btn-inner--text">Github</span>
+                  </Button>
 
-                {/* Kakao Login */}
-                <Button
-                  className="btn-neutral btn-icon"
-                  color="default"
-                  href="#pablo"
-                  onClick={KakaoClick}>
-                  <span className="btn-inner--icon">
-                    <img
-                      alt="..."
-                      src={require("../assets/img/KakaoTalk_logo.svg").default
-                      } />
-                  </span>
-                  <span className="btn-inner--text">
-                    Kakao
-                  </span>
-                </Button>
-              </div>
-            </CardHeader>
+                  {/* Kakao Login */}
+                  <Button
+                    className="btn-neutral btn-icon"
+                    color="default"
+                    href="#pablo"
+                    onClick={KakaoClick}>
+                    <span className="btn-inner--icon">
+                      <img
+                        alt="..."
+                        src={require("../assets/img/KakaoTalk_logo.svg").default
+                        } />
+                    </span>
+                    <span className="btn-inner--text">
+                      Kakao
+                    </span>
+                  </Button>
+                </div>
+              </CardHeader>
           </form>
 
-
+          
 
         </div>
 
-
+        
       </div>
 
     </div>
-</>
+
+    
   );
 }
 
