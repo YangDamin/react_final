c4ca9cc3973a075502101c64f8604f059be5d15a
AwsomePepper <glglwhgdk123@naver.com>
2022-03-18 17:22:07 +0900

    의수

--- src/Main/Write.js
+++ src/Main/Write.js
@@ -8,35 +8,51 @@ import Container from "@mui/material/Container";
 import Header from '../Common/Header';
 import Nav from '../Common/Nav';
 import Footer from '../Common/Footer';
+import './Write.css';
 
 
 
 
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-        ...theme.typography.body2,
-        padding: theme.spacing(14),
-        textAlign: "center",
-        color: theme.palette.text.secondary,
-      }));
-      
-      
-      const Write = () => {
-          return (
-              <>
-              <Header></Header>
-              <Nav></Nav>
-          <CssBaseline />
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(14),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+}));
+
+
+const Write = () => {
+    return (
+        <>
+            <Header></Header>
+            <Nav></Nav>
+            <CssBaseline />
             <Container className="content-container" >
-              <Box sx={{ bgcolor: 'rgba(238, 238, 238, 1)',borderRadius:'40px 40px 0 0',borderStyle:'solid',borderColor:'rgba(153, 153, 153, 1)', height: '100vh' }}>
-            <Box sx={{ flexGrow: 1,mt:6 }}>
-              
-            </Box>
-            </Box>
+                <Box sx={{ bgcolor: 'rgba(238, 238, 238, 1)', borderRadius: '40px 40px 0 0', borderStyle: 'solid', borderColor: 'rgba(153, 153, 153, 1)', height: '100vh' }}>
+                    <Box sx={{ flexGrow: 1, mt: 6 }}>
+
+                        <div className='container'>
+                            <div>
+                            </div>
+                            <div>
+                                <div class="form-group">
+                                    <label for="content">다이어리 작성</label>
+                                    <textarea class="form-control" rows="5" id="content"
+                                        name="content" placeholder="내용 작성"></textarea>
+                                </div>
+
+                                <button type="submit" className="button">등록</button>
+                            </div>
+
+
+                        </div>
+                    </Box>
+                </Box>
             </Container>
             <Footer></Footer>
-          </>
-          );
+        </>
+    );
 }
 
 
