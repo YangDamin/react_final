5232468dc7a0f0606b9cec0dcbd0a3b77ebc6deb
AwsomePepper <glglwhgdk123@naver.com>
2022-04-21 15:21:19 +0900

    buy페이지 수정

--- src/Pay/Buy.js
+++ src/Pay/Buy.js
@@ -6,8 +6,8 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Nav from '../Common/Nav';
 import Payment from './Payment';
-
-
+import { FaBook } from "@react-icons/all-files/fa/FaBook";
+import buy from './buy.jpg'
       
       const Buy = () => {
           return (
@@ -16,10 +16,27 @@ import Payment from './Payment';
           <CssBaseline />
             <Container className="content-container" >
             <Box sx={{ bgcolor: 'rgba(238, 238, 238, 1)',borderRadius:'40px 40px 0 0', 
-			   borderWidth: "5px",borderColor:'black',borderStyle:'solid',
-			   borderColor:'black', height: '100vh',overflow:'overlay' }}>
+                      borderWidth: "5px",borderColor:'black',borderStyle:'solid',
+                      borderColor:'black', height: '100vh',overflow:'overlay' }}>
             <Box sx={{ flexGrow: 1,mt:6 }}>
-              <div></div>
+               <div className="buy-title">
+              <h3 style={{ "fontWeight": "bold","marginTop":"30px" }}>
+                <FaBook />&nbsp; 나의 브이로그 책으로 발간하기&nbsp;<FaBook /></h3></div>
+              <div className="image">
+              <img src={buy} style={{"width":"350px","margin-top":"20px",
+                 "margin-bottom":"20px", "box-shadow":"6px 6px 6px #969696" }}/></div>
+              <div className="buy-content" style={{
+                "marginTop": "10px",
+                "backgroundColor": "white",
+                "borderRadius": "5px 5px 5px 5px",
+                "height": "50px",
+                "textAlign": "center",
+                "overFlow" : "auto",
+                "width" : "400px",
+                "display":"inline-block"
+              }}>
+                나만의 브이로그 다이어리를 책으로 발간하여 간직하세염!!
+              </div>
               <div>
                  <Payment />   
               </div>
